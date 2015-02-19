d9a941da20468974e935ce6b56a462d61d145d9e
Dan Santos <DanielRobertSantos@gmail.com>
2015-02-19 06:28:48 +0000

    Added div to #game

--- public/application.js
+++ public/application.js
@@ -1,53 +1,44 @@
-$(document).ready(function() {
-  player_hits();
-  player_stays();
-  dealer_hits();
+$(document).ready(function(){
+  player_hit();
+  player_stay();
+  dealer_hit();
 });
 
-function player_hits() {
-    $(document).on("click", "form#hit_form input", function() {
-          
-    alert("You hit!");
-    
+function player_hit() {
+  $(document).on("click", "form#hit_form input", function() {
+    alert("You go for the hit!");
     $.ajax({
-      type: "POST",
-      url: "/game/player/hit"
-    });.done(function(msg) {
-      $("#game").replaceWith(msg); //replaces HTML of entire game div with new contents that are returned
+      type: 'POST',
+      url: '/game/player/hit'
+    }).done(function(msg){
+      $("div#game").replaceWith(msg);  //replaces HTML of entire game div with new contents that are returned
     });
-    
     return false;
   });
 }
 
-function player_stays() {
-    $(document).on("click", "form#stay_form input", function() {
-          
+function player_stay() {
+  $(document).on("click", "form#stay_form input", function() {
     alert("You chose to stay!");
-    
     $.ajax({
-      type: "POST",
-      url: "/game/player/stay"
-    });.done(function(msg) {
-      $("#game").replaceWith(msg);
+      type: 'POST',
+      url: '/game/player/stay'
+    }).done(function(msg){
+      $("div#game").replaceWith(msg);
     });
-    
     return false;
   });
 }
 
-function dealer_hits() {
-    $(document).on("click", "form#dealer_hit input", function() {
-          
-    alert("Dealer hits!");
-    
+function dealer_hit() {
+  $(document).on("click", "form#dealer_hit input", function() {
+    alert("The dealer hits!");
     $.ajax({
-      type: "POST",
-      url: "/game/dealer/hit"
-    });.done(function(msg) {
-      $("#game").replaceWith(msg);
+      type: 'POST',
+      url: '/game/dealer/hit'
+    }).done(function(msg){
+      $("div#game").replaceWith(msg);
     });
-    
     return false;
   });
 }
